2eb6d84ef47798cedfcb32309c71ff4af20af4cd
Alnomani <alnomani@protonmail.com>
2023-11-25 16:55:32 +0100

    viteconfig base change docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "https://github.com/Alnomani/DevTimeVisualizer/",
+    base: "https://github.com/Alnomani/DevTimeVisualizer/docs/",
     build: {
         outDir: "./docs",
     },
